d6bb34fc619cb4480845b9a923de35cfcbd83a25
wvq6cy <wvq6cy@virginia.edu>
2025-07-23 20:52:09 -0400

    added tests

--- HW 7/hw7.ml
+++ HW 7/hw7.ml
@@ -56,3 +56,32 @@ assert (postfix_calculator tokens2 = Int 2);
 let tokens3 = [`Num (Int 3); `Num (Int 0); `Op "/"] in try let _ =
 postfix_calculator tokens3 in assert false (* Should raise exception *) with
 Invalid_argument "Division by zero" -> assert true
+
+(* Test 4: Not enough operands => "Not enough operands" *)
+let tokens4 = [`Op "+"; `Num (Int 3)] in
+try let _ = postfix_calculator tokens4 in assert false
+with Invalid_argument "Not enough operands" -> assert true
+
+(*  Test 5: Unknown operator => "Unknown operation" *)
+let tokens5 = [`Num (Int 2); `Num (Int 3); `Op "^"];;
+try let _ = postfix_calculator tokens5 in assert false
+with Invalid_argument "Unknown operation" -> assert true
+  
+(*  test 6: Int + float after  int division => Float 5.5 *)
+let tokens6 = [`Num (Int 5); `Num (Int 2); `Op "/";  
+               `Num (Float 3.5); `Op "+"];;         
+assert (postfix_calculator tokens6 = Float 5.5);
+
+(*  Test 7: Subtraction w negatives, then multiply => Int (-10) *)
+(* 5 10 - 2 *  ==> (5 - 10) = -5;  -5 * 2 = -10 *)
+let tokens7 = [`Num (Int 5); `Num (Int 10); `Op "-"; `Num (Int 2); `Op "*"] in
+assert (postfix_calculator tokens7 = Int (-10));
+
+(*  Test 8: Float division 7.5 2.5 / => Float 3.0 *)
+let tokens8 = [`Num (Float 7.5); `Num (Float 2.5); `Op "/"] in
+assert (postfix_calculator tokens8 = Float 3.0);
+
+(*  Test 9: Malformed expression => "Malformed expression" *)
+let tokens9 = [`Num (Int 3); `Num (Int 4); `Num (Int 5); `Op "+"] in
+try let _ = postfix_calculator tokens9 in assert false
+with Invalid_argument "Malformed expression" -> assert true
